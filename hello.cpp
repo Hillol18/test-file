2760ee1a5a5f089511b1e3554415ce3b52649965
Hillol18 <barikhan.hillol@gmail.com>
2020-01-26 00:30:28 +0600

    Add files via upload
    initial commit

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int ar[100000];
+int sum = 0;
+for(int i=0;i<1000000000;i++){
+  //cout<<"hello"<<endl;
+  sum++;
+ }
+}
+
+
+
+
+
+
+
+
+
+
